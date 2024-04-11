7e57fdfda28b02e65bfa6d104883b4c8d8c87920
gZanda <gabrielfzanda@gmail.com>
2024-04-11 10:47:39 -0300

    Fix

---
+++ K-Means/how_to_plot.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
+# Load the data
+data = pd.read_csv("iris.csv")
+
+# Drop the 'Id' and 'Species' columns
+data.drop(columns=['Id', 'Species'], inplace=True)
+
+# Standardize
+scaler = StandardScaler()
+scaled_data = scaler.fit_transform(data)
+
+# Apply PCA
+pca = PCA(n_components=2)
+pca_result = pca.fit_transform(scaled_data)
+
+# Create a DataFrame 
+pca_df = pd.DataFrame(data=pca_result, columns=['Component 1', 'Component 2'])
+
+# Create plot
+plt.figure(figsize=(8, 6))
+scatter = plt.scatter(pca_df['Component 1'], pca_df['Component 2'], alpha=1, color='blue')
+plt.title('Two dimensional Iris PCA')
+plt.xlabel('Component 1')
+plt.ylabel('Component 2')
+plt.grid(True, which='both', linestyle='--', linewidth=0.5, alpha=0.5)
+
+# Display the plot
+plt.draw()
+plt.pause(2)
+
+# Clear the plot
+plt.clf()
+
+# Update the plot
+scatter = plt.scatter(pca_df['Component 2'], pca_df['Component 1'], alpha=1, color='red')
+plt.title('Two dimensional Iris PCA')
+plt.xlabel('Component 1')
+plt.ylabel('Component 2')
+plt.grid(True, which='both', linestyle='--', linewidth=0.5, alpha=0.5)
+
+# Display
+plt.draw()
+plt.pause(2)
+
+# Clear the plot
+plt.clf()
+
+# Update the plot
+scatter = plt.scatter(pca_df['Component 1'], pca_df['Component 2'], alpha=1, color='green')
+plt.title('Two dimensional Iris PCA')
+plt.xlabel('Component 1')
+plt.ylabel('Component 2')
+plt.grid(True, which='both', linestyle='--', linewidth=0.5, alpha=0.5)
+
+# Display 
+plt.draw()
+
+# Keep displayed until closed
+plt.show()
--- K-Means/k-means.py
+++ K-Means/k-means.py
@@ -2,7 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-import numpy as np
 
 # Load the data
 data = pd.read_csv("iris.csv")
@@ -18,10 +17,10 @@ scaled_data = scaler.fit_transform(data)
 pca = PCA(n_components=2)
 pca_result = pca.fit_transform(scaled_data)
 
-# Create a DataFrame 
+# Create a DataFrame
 pca_df = pd.DataFrame(data=pca_result, columns=['Component 1', 'Component 2'])
 
-# Create plot
+# Create Initial plot
 plt.figure(figsize=(8, 6))
 scatter = plt.scatter(pca_df['Component 1'], pca_df['Component 2'], alpha=1, color='blue')
 plt.title('Two dimensional Iris PCA')
@@ -31,34 +30,10 @@ plt.grid(True, which='both', linestyle='--', linewidth=0.5, alpha=0.5)
 
 # Display the plot
 plt.draw()
-plt.pause(2)
+plt.pause(3)
 
 # Clear the plot
 plt.clf()
 
-# Update the plot
-scatter = plt.scatter(pca_df['Component 2'], pca_df['Component 1'], alpha=1, color='red')
-plt.title('Two dimensional Iris PCA')
-plt.xlabel('Component 1')
-plt.ylabel('Component 2')
-plt.grid(True, which='both', linestyle='--', linewidth=0.5, alpha=0.5)
+# START K-MEANS CLUSTERING
 
-# Display
-plt.draw()
-plt.pause(2)
-
-# Clear the plot
-plt.clf()
-
-# Update the plot
-scatter = plt.scatter(pca_df['Component 1'], pca_df['Component 2'], alpha=1, color='green')
-plt.title('Two dimensional Iris PCA')
-plt.xlabel('Component 1')
-plt.ylabel('Component 2')
-plt.grid(True, which='both', linestyle='--', linewidth=0.5, alpha=0.5)
-
-# Display 
-plt.draw()
-
-# Keep displayed until closed
-plt.show()
